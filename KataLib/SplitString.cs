8061af477169fc25168e6f27e3c6657342f35b90
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-07-30 15:49:41 +0300

    StringSplit

---
+++ KataLib/SplitString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class SplitString
+    {
+        public static string[] Solution(string str)
+        =>
+            (str.Length % 2 == 0
+                ? Enumerable.Range(0, str.Length / 2).Select(i => str.Substring(i * 2, 2))
+                : Solution(str.Substring(0, str.Length - 1)).Append(str.Substring(str.Length - 1) + "_"))
+                    .ToArray();
+    }
+}
---
+++ KataTests/SplitString.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class SplitStringTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new string[] { "ab", "c_" }, SplitString.Solution("abc"));
+            Assert.AreEqual(new string[] { "ab" }, SplitString.Solution("ab"));
+            Assert.AreEqual(new string[] { "ab", "cd", "ef" }, SplitString.Solution("abcdef"));
+        }
+    }
+}
