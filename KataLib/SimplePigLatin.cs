257d7dbba8e325aa4546d5b0c803a3d27760b4c6
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-07-31 15:17:53 +0300

    Pig Latin

---
+++ KataLib/SimplePigLatin.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KataLib
+{
+    public partial class Kata
+    {
+        public static string PigIt(string str)
+        =>
+            Regex.Replace(str, @"((\S)(\S+))", "$3$2ay");
+    }
+}
