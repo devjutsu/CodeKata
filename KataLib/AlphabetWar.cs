cab942f8e32663725a68d74d938ee281975b8d07
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-07-31 13:33:13 +0300

    AlphabetWar

---
+++ KataLib/AlphabetWar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public partial class Kata
+    {
+        public static string AlphabetWar(string fight)
+        =>
+            Message(fight
+                        .ToList()
+                        .Select(c => CountChar(c))
+                        .Sum());
+
+        public static string Message(int score)
+        =>
+            score == 0
+                ? "Let's fight again!"
+                : score > 0
+                    ? "Right side wins!"
+                    : "Left side wins!";
+
+
+        public static int CountChar(char c)
+        =>
+            c switch
+            {
+                'w' => -4,
+                'p' => -3,
+                'b' => -2,
+                's' => -1,
+                'm' => 4,
+                'q' => 3,
+                'd' => 2,
+                'z' => 1,
+                _ => 0
+            };
+    }
+}
---
+++ KataTests/AlphabetWar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class AlphabetWar
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual("Left side wins!", Kata.AlphabetWar("wwwwwwz"));
+            Assert.AreEqual("Let's fight again!", Kata.AlphabetWar("ffffff"));
+            Assert.AreEqual("Let's fight again!", Kata.AlphabetWar(""));
+            Assert.AreEqual("Right side wins!", Kata.AlphabetWar("z"));
+            Assert.AreEqual("Let's fight again!", Kata.AlphabetWar("zdqmwpbs"));
+            Assert.AreEqual("Right side wins!", Kata.AlphabetWar("zzzzs"));
+            Assert.AreEqual("Left side wins!", Kata.AlphabetWar("wwwwwwz"));
+        }
+    }
+}
