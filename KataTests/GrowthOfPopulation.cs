38e47d17ab4ec6edd24cedde7a4b7ee8353f4236
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-01 14:34:03 +0300

    Growth of a Population

---
+++ KataTests/GrowthOfPopulation.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    [TestFixture]
+    public static class ArgeTests
+    {
+
+        private static void testing(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing NbYear");
+            testing(Arge.NbYear(1500, 5, 100, 5000), 15);
+            testing(Arge.NbYear(1500000, 2.5, 10000, 2000000), 10);
+            testing(Arge.NbYear(1500000, 0.25, 1000, 2000000), 94);
+        }
+    }
+}
