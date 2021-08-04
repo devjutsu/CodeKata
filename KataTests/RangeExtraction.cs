c85c01b74cfa8e981019c91abec142ea75ae22ac
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-04 15:51:37 +0300

    Very ugly solution of RangeExtraction

---
+++ KataTests/RangeExtraction.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    public class RangeExtractionTests
+    {
+        [Test]
+        public void CheckRangeExtraction()
+        {
+            Assert.AreEqual(
+                  "-6,-3-1,3-5,7-11,14,15,17-20,22,23",
+                  RangeExtraction.Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20, 22,23 })
+              );
+        }
+
+        [Test]
+        public void SimpleTests()
+        {
+
+            Assert.AreEqual("1,2", RangeExtraction.Extract(new[] { 1, 2 }));
+            Assert.AreEqual("1-3", RangeExtraction.Extract(new[] { 1, 2, 3 }));
+
+            Assert.AreEqual("12,13,15-17", RangeExtraction.Extract(new[] { 12, 13, 15, 16, 17 }));
+
+            Assert.AreEqual(
+                "-6,-3-1,3-5,7-11,14,15,17-20",
+                RangeExtraction.Extract(new[] { -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20 })
+            );
+
+            Assert.AreEqual(
+                "-3--1,2,10,15,16,18-20",
+                RangeExtraction.Extract(new[] { -3, -2, -1, 2, 10, 15, 16, 18, 19, 20 })
+            );
+        }
+    }
+}
