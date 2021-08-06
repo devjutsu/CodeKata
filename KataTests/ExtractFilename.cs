8fc8f500b54e7e9c99df437e9e74c11c742294c1
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-06 10:37:59 +0300

    ExtractFilename, RangeExtraction refactored

---
+++ KataTests/ExtractFilename.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class ExtractFilename
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual("FILE_NAME.EXTENSION", FileNameExtractor.ExtractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"));
+            Assert.AreEqual("FILE_NAME.EXTENSION", FileNameExtractor.ExtractFileName("1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION"));
+        }
+    }
+}
