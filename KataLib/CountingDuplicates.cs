17d39d77a0b03108e9d78a862ec947dff1dcfee3
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-08 15:10:19 +0300

    Duplicate count functional

---
+++ KataLib/CountingDuplicates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public partial class Kata
+    {
+        public static int DuplicateCount(string str)
+            => str.ToLower().GroupBy(c => c).Count(c => c.Count() > 1);
+
+        //public static int DuplicateCountBad(string str)
+        //    => RecursiveCount(str.ToLower(), "", "");
+
+        //public static int RecursiveCount(string str, string already, string duplicates)
+        //    => str.Length == 0
+        //            ? duplicates.Length
+        //            : already.Contains(str.First())
+        //                ? duplicates.Contains(str.First())
+        //                    ? RecursiveCount(str.Substring(1), already, duplicates)
+        //                    : RecursiveCount(str.Substring(1), already, duplicates + str.First())
+        //                : RecursiveCount(str.Substring(1), already + str.First(), duplicates);
+    }
+}
---
+++ KataTests/CountingDuplicates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    public class CountingDuplicates
+    {
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual(0, Kata.DuplicateCount(""));
+            Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
