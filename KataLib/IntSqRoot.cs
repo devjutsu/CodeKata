bed8d0cc97ecbf95bfa9a776f65fde140e12022d
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-11 10:21:15 +0300

    Int square root

---
+++ KataLib/IntSqRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class IntSqRoot
+    {
+        public static long IntRac(this long n, long x)
+            => x == (int)Math.Sqrt(n)
+                    ? 1
+                    : 1 + n.IntRac((x + n / x) / 2);
+    }
+}
