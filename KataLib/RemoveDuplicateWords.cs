468ed2c4fd350a27d163c4fa596ca2a4472f6d31
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-14 09:36:36 +0300

    Remove duplicate words

---
+++ KataLib/RemoveDuplicateWords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static partial class RemoveDuplicateWordsKata
+    {
+        public static string RemoveDuplicateWords(string s)
+            => string.Join("", s.Split()
+                                    .GroupBy(o => o)
+                                    .Select(o => o.Key));
+
+
+    }
+}
---
+++ KataTests/RemoveDuplicateWords.cs
@@ -0,0 +1,19 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class RemoveDuplicateWords
+    {
+        [Test, Description("Should handle sample case")]
+        public void ExampleTest()
+        {
+            Assert.That(RemoveDuplicateWordsKata.RemoveDuplicateWords("alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"), Is.EqualTo("alpha beta gamma delta"));
+        }
+    }
+}
