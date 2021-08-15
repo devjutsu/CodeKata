c1066405e63f9ff945be8f0b034601397ca0e2f9
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-15 09:47:56 +0300

    Sum of multiplies by 3 or 5

---
+++ KataLib/Multiplies3or5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Multiplies3or5
+    {
+        public static int Solution(int value)
+            => RecursiveFind(value - 1);
+
+        public static int RecursiveFind(int n)
+        => n < 3
+            ? 0
+            : n % 3 == 0 || n % 5 == 0
+                ? n + RecursiveFind(n - 1)
+                : RecursiveFind(n - 1);
+
+        // ------------------------------------------
+        public static int Solution2(int n)
+            => Enumerable.Range(0, n)
+                            .Where(e => e % 3 == 0 || e % 5 == 0)
+                            .Sum();
+    }
+}
---
+++ KataTests/Multiplies3or5.cs
@@ -0,0 +1,20 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class Multiplies3or5Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, Multiplies3or5.Solution(10));
+        }
+    }
+}
