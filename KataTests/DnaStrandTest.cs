8d9d9d320f7fcbc6e856753d671db2543e4c0f14
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-17 15:13:43 +0300

    Complementary DNA

---
+++ KataTests/DnaStrandTest.cs
@@ -0,0 +1,32 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class DnaStrandTest
+    {
+        [TestCase("AAAA", "TTTT")]
+        [TestCase("ATTGC", "TAACG")]
+        [TestCase("GTAT", "CATA")]
+        [TestCase("AAGG", "TTCC")]
+        [TestCase("CGCG", "GCGC")]
+        [TestCase("ATTGC", "TAACG")]
+        [TestCase("GTATCGATCGATCGATCGATTATATTTTCGACGAGATTTAAATATATATATATACGAGAGAATACAGATAGACAGATTA", "CATAGCTAGCTAGCTAGCTAATATAAAAGCTGCTCTAAATTTATATATATATATGCTCTCTTATGTCTATCTGTCTAAT")]
+        public void SampleTests(string dna, string expected)
+        {
+            Assert.AreEqual(expected, DnaStrand.MakeComplement(dna));
+        }
+
+        public void Bzzdate()
+        {
+            var testdate = DateTime.Now.AddDays(73).ToShortDateString();
+
+            return;
+        }
+    }
+}
