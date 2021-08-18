f4418fcf9b82ea85bf264418a0afa32dfd81bf0e
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-18 13:21:53 +0300

    WeirdIPv6Parsing

---
+++ KataLib/WeirdIPv6Parsing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace KataLib
+{
+    public static class WeirdIPv6Parsing
+    {
+        public static string ParseIPv6(string iPv6)
+            => iPv6.Split(iPv6[4])
+                    .Select(o => o.CountOctet())
+                    .Stringify();
+
+        public static string Stringify(this IEnumerable<int> source)
+            => string.Join("", source);
+
+        public static int CountOctet(this string octet)
+            => octet.Select(o => Convert.ToInt32(o.ToString(), 16)).Sum();
+
+        //=> octet.Length == 0
+        //    ? 0
+        //    : CountOctet(octet.Substring(1))
+        //        + Convert.ToInt32(octet[0].ToString(), 16);
+
+        // --------------------------------------------------
+
+        //public static string ParseIPv6(string ip) =>
+        //        string.Concat(Regex.Matches(ip, @"[0-9A-F]+").Select(m => m.Value.Sum(c => "0123456789ABCDEF".IndexOf(c))));
+
+
+        //// --------------------------------------------------
+
+        //public static string ParseIPv6(string iPv6)
+        //{
+        //    return string.Join(string.Empty,
+        //        new Regex(@"([0-9a-fA-F]+)")
+        //            .Matches(iPv6)
+        //            .Select(match => match.Value.Select(Uri.FromHex).Sum())
+        //    );
+        //}
+
+        //// ---------------------------------------------------
+
+        //public static string ParseIPv6(string iPv6)
+        //{
+        //    return string.Concat(iPv6.Split(iPv6[4]).Select(x => x.Sum(y => Convert.ToInt32($"{y}", 16))));
+        //}
+    }
+}
---
+++ KataTests/WeirdIPv6Parsing.cs
@@ -0,0 +1,30 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests.Folder
+{
+    public class WeirdIPv6ParsingTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("10264228481217", WeirdIPv6Parsing.ParseIPv6("1234:5678:9ABC:D00F:1111:2222:3333:4445"));
+            Assert.AreEqual("18544240608532", WeirdIPv6Parsing.ParseIPv6("5454:FBFD:9ABC:AAAA:FFFF:2222:FBDE:0101"));
+            Assert.AreEqual("00000000", WeirdIPv6Parsing.ParseIPv6("0000:0000:0000:0000:0000:0000:0000:0000"));
+            Assert.AreEqual("6060444864304", WeirdIPv6Parsing.ParseIPv6("FFFF:FFFF:BBBB:CCCC:1212:AABC:0000:1111"));
+            Assert.AreEqual("48242406085346", WeirdIPv6Parsing.ParseIPv6("ACDD-0101-9ABC-AAAA-FFFF-2222-FBDE-ACCC"));
+        }
+
+        [Test]
+        public void AdvancedTests()
+        {
+            Assert.AreEqual("18544230558532", WeirdIPv6Parsing.ParseIPv6("5454rFBFDr9ABCrAA0ArFAFFr2222rFBDEr0101"));
+            Assert.AreEqual("24264228481221", WeirdIPv6Parsing.ParseIPv6("F234#5678#9ABC#D00F#1111#2222#3333#4485"));
+        }
+    }
+}
