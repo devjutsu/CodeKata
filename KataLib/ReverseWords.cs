4ea81a52c34306bad2f8af31d4a3a4934bf87518
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-24 13:17:14 +0300

    Reverse words

---
+++ KataLib/ReverseWords.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class ReverseThem
+    {
+        public static string ReverseWords(string str)
+            => string.Join(" ", str.Split(' ').Select(x => string.Concat(x.Reverse())));
+    }
+}
---
+++ KataTests/ReverseWords.cs
@@ -0,0 +1,19 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class ReverseWordsTest
+    {
+        [Test]
+        public void Example()
+        {
+            Assert.AreEqual("sihT si na !elpmaxe", ReverseThem.ReverseWords("This is an example!"));
+        }
+    }
+}
