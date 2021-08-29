86d2049171783932829545d60ef53c294bb86ea9
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-08-29 14:39:19 +0300

    Highest scoring word

---
+++ KataLib/HighestScoringWord.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace KataLib
+{
+    public static class HighestScoringWord
+    {
+        public static string High(string s)
+            => s.Split()
+                    .Select(x => new { Count = x.WordCount(), Word = x })
+                    .OrderByDescending(x => x.Count)
+                    .Select(x => x.Word)
+                    .First();
+
+        public static int WordCount(this string word)
+            => word.Select(x => (char)x - 96).Sum();
+
+        //public static string High(string s)
+        //{
+        //    return s.Split(' ').OrderBy(a => a.Select(b => b - 96).Sum()).Last();
+        //}
+    }
+}
---
+++ KataTests/HighestScoringWord.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    public class HighestScoringWordTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("man i need a taxi up to ubud").Returns("taxi");
+                yield return new TestCaseData("what time are we climbing up to the volcano").Returns("volcano");
+                yield return new TestCaseData("take me to semynak").Returns("semynak");
+                yield return new TestCaseData("aa b").Returns("aa");
+                yield return new TestCaseData("b aa").Returns("b");
+                yield return new TestCaseData("bb d").Returns("bb");
+                yield return new TestCaseData("d bb").Returns("d");
+                yield return new TestCaseData("aaa b").Returns("aaa");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string s) => HighestScoringWord.High(s);
+}
+}
