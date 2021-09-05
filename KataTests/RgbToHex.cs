8be5d8105d345f082f117e4ac51c226a552c727f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-05 11:20:56 +0300

    RgbToHex

---
+++ KataTests/RgbToHex.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+
+namespace KataTests
+{
+    public class RgbToHexTest
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("FFFFFF", RgbToHex.Rgb(255, 255, 255));
+            Assert.AreEqual("FFFFFF", RgbToHex.Rgb(255, 255, 300));
+            Assert.AreEqual("000000", RgbToHex.Rgb(0, 0, 0));
+            Assert.AreEqual("9400D3", RgbToHex.Rgb(148, 0, 211));
+            Assert.AreEqual("9400D3", RgbToHex.Rgb(148, -20, 211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", RgbToHex.Rgb(144, 195, 212));
+            Assert.AreEqual("D4350C", RgbToHex.Rgb(212, 53, 12), "Consider single hex digit numbers.");
+        }
+    }
+}
