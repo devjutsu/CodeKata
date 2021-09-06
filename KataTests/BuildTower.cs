503cd1b6e370efda6f2fc5fe715f026e7f17c9d4
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-06 17:56:53 +0300

    Build tower

---
+++ KataTests/BuildTower.cs
@@ -0,0 +1,21 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class BuildTowerTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(string.Join(",", new[] { "*" }), string.Join(",", BuildTower.TowerBuilder(1)));
+            Assert.AreEqual(string.Join(",", new[] { " * ", "***" }), string.Join(",", BuildTower.TowerBuilder(2)));
+            Assert.AreEqual(string.Join(",", new[] { "  *  ", " *** ", "*****" }), string.Join(",", BuildTower.TowerBuilder(3)));
+        }
+    }
+}
