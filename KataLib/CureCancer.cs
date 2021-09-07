b88740944311f68ac85f14cdb9f7eabfc8398fbe
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-07 16:28:57 +0300

    Cure Cancer JomoPipi

---
+++ KataLib/CureCancer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class CureCancer
+    {
+        public static int[] MutationLocation(char[][] body)
+            => Enumerable.Range(0, body[0].Length)
+                .Select(i => body.Position(i))
+                .Select((o, i) => new { Row = i, Pos = o })
+                .Where(o => o.Pos != null)
+                ?.Select(o => new int[] { o.Pos.Value, o.Row })
+                ?.SingleOrDefault()
+                ?? Array.Empty<int>();
+
+        public static int? Position(this char[][] body, int n)
+            => body.Select((x, i) => new { C = x[n], I = i })
+                    .GroupBy(x => x.C)
+                    .SingleOrDefault(x => x.Count() == 1)
+                    ?.Select(o => o.I)
+                    ?.SingleOrDefault()
+                    ?? null;
+    }
+}
---
+++ KataTests/CureCancer.cs
@@ -0,0 +1,52 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class CureCancerTest
+    {
+        [Test]
+        public void TestSomething1()
+        {
+            char[][] body =
+            {
+            "thisIsAsmallerPerson".ToCharArray(),
+            "thisIsAsmallerPerson".ToCharArray(),
+            "thisIsAsmallerPerson".ToCharArray(),
+            "thisssAsmallerPerson".ToCharArray()
+        };
+            int[] answer = CureCancer.MutationLocation(body);
+            Assert.AreEqual(new int[] { 3, 4 }, answer);
+        }
+        [Test]
+        public void TestSomething2()
+        {
+            char[][] body =
+            {
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuff1thinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray(),
+            "someGreaTstuffIthinkThisIs".ToCharArray()
+        };
+            int[] answer = CureCancer.MutationLocation(body);
+            Assert.AreEqual(new int[] { 9, 14 }, answer);
+        }
+    }
+}
