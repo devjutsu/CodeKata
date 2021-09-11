6d32b5dd91891b6662973d83956559d5f48a6e1a
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-11 18:59:52 +0300

    Moves in squared strings (I)

---
+++ KataTests/Opstrings.cs
@@ -0,0 +1,31 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class OpstringsTest
+    {
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Fixed Tests VertMirror");
+            testing(Opstrings.Oper(Opstrings.VertMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu"),
+                "QHdgSh\noaMDnH\nXxNNlC\nHxxvRi\nAvVTqb\nuRySvw");
+            testing(Opstrings.Oper(Opstrings.VertMirror, "IzOTWE\nkkbeCM\nWuzZxM\nvDddJw\njiJyHF\nPVHfSx"),
+                "EWTOzI\nMCebkk\nMxZzuW\nwJddDv\nFHyJij\nxSfHVP");
+
+            Console.WriteLine("Fixed Tests HorMirror");
+            testing(Opstrings.Oper(Opstrings.HorMirror, "lVHt\nJVhv\nCSbg\nyeCt"), "yeCt\nCSbg\nJVhv\nlVHt");
+            testing(Opstrings.Oper(Opstrings.HorMirror, "njMK\ndbrZ\nLPKo\ncEYz"), "cEYz\nLPKo\ndbrZ\nnjMK");
+        }
+    }
+}
