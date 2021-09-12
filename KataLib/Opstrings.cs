732ad2588598b6ac544e2be7492d9ca27384fbc7
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-12 13:23:18 +0300

    Moves in squared strings (II)

--- KataLib/Opstrings.cs
+++ KataLib/Opstrings.cs
@@ -8,11 +8,26 @@ namespace KataLib
 {
     public static class Opstrings
     {
-        public static string[] VertMirror(string[] src)
+        public static string[] VertMirror(this string[] src)
             => src.Select(o => string.Join("", o.Reverse())).ToArray();
 
-        public static string[] HorMirror(string[] src)
+        public static string[] HorMirror(this string[] src)
             => src.Select((o, i) => src[src.Length - 1 - i]).ToArray();
+
+        public static string Oper(Func<string[], string[]> op, string src)
+            => string.Join("\n", op(src.Split("\n")));
+    }
+
+    public static class Opstrings1
+    {
+        public static string[] Rot(this string[] src)
+            => src.Select((o, i) => string.Join("", src[src.Length - 1 - i].Reverse()))
+                .ToArray();
+
+        public static string[] SelfieAndRot(this string[] src)
+            => src.Select(o => o + new string('.', o.Length))
+                        .Concat(src.Rot().Select(o => new string('.', o.Length) + o))
+                        .ToArray();
 
         public static string Oper(Func<string[], string[]> op, string src)
             => string.Join("\n", op(src.Split("\n")));
