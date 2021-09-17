ba106d1a6f5d3df78437ff8a1121a2f774ea3947
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-17 23:53:38 +0300

    Bowling

---
+++ KataTests/Bowling.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    public class BowlingTest
+    {
+        [Test]
+        public void exampleTests()
+        {
+            Bowling bowling = new Bowling();
+            int[] testArray = new int[] { 1, 2, 3 };
+            Assert.AreEqual("I I I I\n I I I \n       \n       ", bowling.BowlingPins(testArray));
+
+            testArray = new int[] { 3, 5, 9 };
+            Assert.AreEqual("I I   I\n I   I \n  I    \n   I   ", bowling.BowlingPins(testArray));
+        }
+    }
+}
