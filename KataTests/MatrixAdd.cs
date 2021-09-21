50ff39abc20bf1646ac70d7cdaab718591f440ef
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-22 00:53:45 +0300

    Matrix addition

---
+++ KataTests/MatrixAdd.cs
@@ -0,0 +1,23 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    public class MatrixAdditionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[][] {new int[] {3, 5},
+                                   new int[] {3, 5}}, MatrixAdd.MatrixAddition(new int[][] {new int[] {1, 2},
+                                                                                       new int[] {1, 2}},
+                                                                                             new int[][] {new int[] {2, 3},
+                                                                                                    new int[] {2, 3}}));
+        }
+    }
+}
