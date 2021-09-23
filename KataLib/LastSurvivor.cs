3df4931e142dc7be2619be25ef044a48cf3acbd9
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-23 21:42:50 +0300

    Last survivor

---
+++ KataLib/LastSurvivor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class LastSurvivorz
+    {
+        public static string LastSurvivor(string letters, int[] coords)
+        => coords.Length > 0
+                ? LastSurvivor(letters.Remove(coords[0], 1), coords.Skip(1).ToArray())
+                : letters;
+
+
+        // return coords.Aggregate(letters, (s, i) => s[..i] + s[(i + 1)..]);
+    }
+}
