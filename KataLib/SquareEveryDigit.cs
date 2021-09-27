a6dfa39657edf73a2c53e82cfd7d15f9127faed4
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-27 18:09:52 +0300

    Square every digit

---
+++ KataLib/SquareEveryDigit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class SquareEveryDigit
+    {
+        public static int SquareDigits(int n)
+            => int.Parse(string.Join("", 
+                n.ToString().Select(o => int.Parse(o.ToString())).Select(o => o * o)));
+    }
+}
