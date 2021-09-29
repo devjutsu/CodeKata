62e8828efbf175fa07e8d90a473e496f9c5762c2
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-29 18:42:10 +0300

    Words in triangle

---
+++ KataTests/WordsInTriangle.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using NUnit;
+using KataLib;
+
+namespace Exercises.Level2.Tests
+{
+    public class WordsInTriangleTests
+    {
+        private readonly WordsInTriangle _wordsInTriangle = new WordsInTriangle();
+
+        [Test]
+        public void Hello()
+        {
+            string[] expected = new string[]
+            {
+                "     *",
+                "    * *",
+                "   *   *",
+                "  *     *",
+                " * home  *",
+                "***********",
+            };
+
+            string[] words = new string[] { "home" };
+
+            Assert.AreEqual(expected, _wordsInTriangle.GetWordInTriangle(words));
+        }
+
+        [Test]
+        public void ABeautifulHome()
+        {
+            string[] expected = new string[]
+            {
+                "        *",
+                "       * *",
+                "      *   *",
+                "     *     *",
+                "    *       *",
+                "   * a       *",
+                "  * beautiful *",
+                " * home        *",
+                "*****************",
+            };
+
+            string[] words = new string[] { "a", "beautiful", "home" };
+
+            Assert.AreEqual(expected, _wordsInTriangle.GetWordInTriangle(words));
+        }
+
+        [Test]
+        public void SunshineiSgREAT()
+        {
+            string[] expected = new string[]
+            {
+                "         *",
+                "        * *",
+                "       *   *",
+                "      *     *",
+                "     *       *",
+                "    *         *",
+                "   * sunshine  *",
+                "  * is          *",
+                " * great         *",
+                "*******************",
+            };
+
+            string[] words = new string[] { "sunshine", "is", "great" };
+
+            Assert.AreEqual(expected, _wordsInTriangle.GetWordInTriangle(words));
+        }
+
+        [Test]
+        public void A()
+        {
+            string[] expected = new string[]
+            {
+                "   *",
+                "  * *",
+                " * A *",
+                "*******",
+            };
+
+            string[] words = new string[] { "A" };
+
+            Assert.AreEqual(expected, _wordsInTriangle.GetWordInTriangle(words));
+        }
+
+        [Test]
+        public void Empty()
+        {
+            string[] expected = new string[]
+            {
+                "  *",
+                " * *",
+                "*****",
+            };
+
+            string[] words = new string[] { };
+
+            Assert.AreEqual(expected, _wordsInTriangle.GetWordInTriangle(words));
+        }
+    }
+}
