b7f004e117e16589ff235931fd195c3a68485928
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-09-30 23:27:44 +0300

    Sum of odd qubes

---
+++ KataLib/SumOfOddQubes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class SumOfOddQubes
+    {
+        public static int CubeOdd(int[] arr)
+            => arr.Where(o => Math.Abs(o) % 2 == 1).Select(o => o * o * o).Sum();
+    }
+}
