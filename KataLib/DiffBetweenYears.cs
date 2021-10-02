acec7522327ef5f5ee28cf418634badc80d85b1a
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-02 18:31:53 +0300

    Diff between years

---
+++ KataLib/DiffBetweenYears.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class DiffBetweenYears
+    {
+        public static int HowManyYears(string date1, string date2)
+            => Math.Abs(DateTime.Parse(date1).Year - DateTime.Parse(date2).Year);
+    }
+}
---
+++ KataTests/DiffBetweenYears.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class DiffBetweenYearsKata
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(18, DiffBetweenYears.HowManyYears("1997/10/10", "2015/10/10"));
+            Assert.AreEqual(25, DiffBetweenYears.HowManyYears("1990/10/10", "2015/10/10"));
+            Assert.AreEqual(25, DiffBetweenYears.HowManyYears("2015/10/10", "1990/10/10"));
+            Assert.AreEqual(23, DiffBetweenYears.HowManyYears("1992/10/24", "2015/10/24"));
+            Assert.AreEqual(18, DiffBetweenYears.HowManyYears("2018/10/10", "2000/10/10"));
+            Assert.AreEqual(0, DiffBetweenYears.HowManyYears("2000/10/10", "2000/10/10"));
+        }
+    }
+}
