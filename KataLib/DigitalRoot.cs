dd2d7e47829e73c355a84501addeb37fc4d839c4
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-04 17:55:07 +0300

    DigitalRoot

---
+++ KataLib/DigitalRoot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class DigitalRootSum
+    {
+        public int DigitalRoot(long n)
+            => n.ToString().Length > 1 
+                    ? DigitalRoot(Sum(n.ToString()))
+                    : (int)n;
+
+        public int Sum(string s)
+            => s.Select(o => int.Parse(o.ToString())).Sum();
+    }
+
+    //public long DigitalRoot(long n) => (n - 1) % 9 + 1;
+}
---
+++ KataTests/DigitalRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using KataLib;
+
+namespace KataTests
+{
+    internal class DigitalRootTest
+    {
+        private DigitalRootSum num;
+
+        [SetUp]
+        public void SetUp()
+        {
+            num = new DigitalRootSum();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            num = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(7, num.DigitalRoot(16));
+            Assert.AreEqual(6, num.DigitalRoot(456));
+        }
+    }
+}
