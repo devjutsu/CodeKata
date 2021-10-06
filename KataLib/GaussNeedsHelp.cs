b061daf7963abef392bfbba2f8d26ef78cf6d09a
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-06 21:52:55 +0300

    Gauss needs help

---
+++ KataLib/GaussNeedsHelp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class GaussNeedsHelp
+    {
+        //public static long? RangeSum(long n)
+        //{ if (n > 0) return n * (n + 1) / 2; else return null; }
+
+        public static long? RangeSum(long n)
+            => n > 0 ? (n * (n + 1) / 2) : 0;
+    }
+}
---
+++ KataTests/GaussNeedsHelp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class GaussNeedsHelpTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(100l).Returns(5050l);
+                yield return new TestCaseData(300l).Returns(45150l);
+                yield return new TestCaseData(50000l).Returns(1250025000l);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public long? Test(long n) => GaussNeedsHelp.RangeSum(n);
+    }
+}
