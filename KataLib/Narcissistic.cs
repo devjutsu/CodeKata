c65555e1abe6c80e889195a98433e703afc058a7
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-08 16:56:21 +0300

    Narcissistic

---
+++ KataLib/Narcissistic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class NarcissisticKata
+    {
+        public static bool Narcissistic(int value)
+        => value.ToString()
+                    .Select(o => int.Parse(o.ToString()))
+                    .Select(n => Math.Pow(n, value.ToString().Length))
+                    .Sum() == value;
+    }
+}
---
+++ KataTests/Narcissistic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class NarcissisticTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1)
+                                .Returns(true)
+                                .SetDescription("1 is narcissitic");
+                yield return new TestCaseData(371)
+                                .Returns(true)
+                                .SetDescription("371 is narcissitic");
+
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(int n) => NarcissisticKata.Narcissistic(n);
+    }
+}
