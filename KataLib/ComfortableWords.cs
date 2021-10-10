3e6be59526b1c2049b0a6644735421939b1ebd7f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-10 16:30:28 +0300

    Comfortable words

---
+++ KataLib/ComfortableWords.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class ComfortableWords
+    {
+        public static string left = "q, w, e, r, t, a, s, d, f, g, z, x, c, v, b";
+        public static string right = "y, u, i, o, p, h, j, k, l, n, m";
+        public static bool ComfortableWord(string word)
+        => left.Contains(word.First())
+                      ? word.Comfy(left, true) && word.Comfy(right, false)
+                      : word.Comfy(left, false) && word.Comfy(right, true);
+
+        public static bool Comfy(this string word, string side, bool even)
+        => word.Select((o, i) => new { Index = i, Char = o })
+                    .Where(o => o.Index % 2 == (even ? 0 : 1))
+                    .All(o => side.Contains(o.Char));
+
+    }
+
+    //public bool ComfortableWord(string word)
+    //{
+    //    var s = string.Concat(word.Select(c => "yuiophjklnm".Contains(c) ? "R" : "L"));
+    //    return !(s.Contains("LL") || s.Contains("RR"));
+    //}
+
+    //public bool ComfortableWord(string word)
+    //{
+    //    return !Regex.IsMatch(
+    //      Regex.Replace(word, @"q|w|e|r|t|a|s|d|f|g|z|x|c|v|b", " "), @"\s{2}|\w{2}"
+    //    );
+    //}
+}
---
+++ KataTests/ComfortableWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class ComfortableWordsTest
+    {
+        [Test]
+        public void ConfortableWords()
+        {
+            Assert.AreEqual(true, ComfortableWords.ComfortableWord("yams"), "yams is a confortable word");
+            Assert.AreEqual(true, ComfortableWords.ComfortableWord("odor"), "odor is a confortable word");
+            Assert.AreEqual(true, ComfortableWords.ComfortableWord("their"), "their is a confortable word");
+        }
+
+        [Test]
+        public void NotConfortableWords()
+        {
+            Assert.AreEqual(false, ComfortableWords.ComfortableWord("leisure"), "leisure is NOT a confortable word");
+            Assert.AreEqual(false, ComfortableWords.ComfortableWord("touts"), "touts is NOT a confortable word");
+            Assert.AreEqual(false, ComfortableWords.ComfortableWord("test"), "test is NOT a confortable word");
+        }
+    }
+}
