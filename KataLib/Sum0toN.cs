d5e5e5dd08d916593b7cce8d2fa709b3f97a2765
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-12 12:30:15 +0300

    Sum 0 to N

---
+++ KataLib/Sum0toN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Sum0toN
+    {
+        public static string ShowSequence(int n)
+        {
+            if (n == 0)
+                return "0=0";
+            else if (n < 0)
+                return $"{n}<0";
+
+            var seq = Enumerable.Range(0, n + 1).ToList();
+            return $"{string.Join("+", seq)} = {seq.Sum()}";
+        }
+    }
+}
---
+++ KataTests/Sum0toN.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using KataLib;
+
+namespace KataTests
+{
+    public class Sum0toNTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("0+1+2+3+4+5+6 = 21", Sum0toN.ShowSequence(6));
+        }
+    }
+}
