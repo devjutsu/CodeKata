791e4238ba8239aaa01a86bad4edf0dfcde80267
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-16 17:52:47 +0300

    Find the stray

---
+++ KataLib/FindTheStray.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class FindTheStray
+    {
+        public static int Stray(int[] n)
+        => n.GroupBy(o => o).First(o => o.Count() == 1).Key;
+    }
+}
