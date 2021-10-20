7c26a8af71f8a31b7c6bee33eaaf007ffad3d642
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-20 22:27:51 +0300

    Return missing

---
+++ KataLib/ReturnMissing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class ReturnMissing
+    {
+        public static int GetMissingElement(int[] superImportantArray)
+        => 45 - superImportantArray.Sum();
+    }
+}
---
+++ KataTests/ReturnMissing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using KataLib;
+
+namespace KataTests
+{
+    internal class ReturnMissingTest
+    {
+        [Test]
+        public void EightIsMissing()
+        {
+            Assert.AreEqual(8, ReturnMissing.GetMissingElement(new int[9] { 0, 5, 1, 3, 2, 9, 7, 6, 4 }));
+        }
+
+        [Test]
+        public void ThreeIsMissing()
+        {
+            Assert.AreEqual(3, ReturnMissing.GetMissingElement(new int[9] { 9, 2, 4, 5, 7, 0, 8, 6, 1 }));
+        }
+    }
+}
