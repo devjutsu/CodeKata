15bb2f269950bb2b947deb57dd7c8435722bcd1e
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-21 23:18:41 +0300

    Two to one

---
+++ KataLib/TwoToOne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class TwoToOne
+    {
+        public static string Longest(string s1, string s2)
+        => string.Join("", (s1 + s2).Distinct().OrderBy(o => o));
+    }
+}
