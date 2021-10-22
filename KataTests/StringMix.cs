ebd8304f102d2f7c8c55f7a05f59d7e1eabd06c8
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-22 21:17:33 +0300

    String mix

---
+++ KataTests/StringMix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class StringMixTest
+    {
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual("2:eeeee/2:yy/=:hh/=:rr", StringMix.Mix("Are they here", "yes, they are here"));
+
+            Assert.AreEqual("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
+                    StringMix.Mix("looping is fun but dangerous", "less dangerous than coding"));
+
+
+            Assert.AreEqual("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
+                    StringMix.Mix(" In many languages", " there's a pair of functions"));
+
+
+            Assert.AreEqual("1:ee/1:ll/1:oo", StringMix.Mix("Lords of the Fallen", "gamekult"));
+
+
+            Assert.AreEqual("", StringMix.Mix("codewars", "codewars"));
+
+
+            Assert.AreEqual("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
+                    StringMix.Mix("A generation must confront the looming ", "codewarrs"));
+        }
+    }
+}
