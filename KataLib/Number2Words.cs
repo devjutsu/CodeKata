16078b53622a348b710836acd35e96fec2ea32d8
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-23 21:45:56 +0300

    number 2 words

---
+++ KataLib/Number2Words.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class NumberTranslation
+    {
+        static string[] map = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        static string[] tens = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+        public static string Number2Words(int n) // 0 - 999999
+        {
+            if (n == 0)
+                return "zero";
+
+            string words = "";
+
+            if(n / 1000 > 0)
+            {
+                words += Number2Words(n / 1000) + " thousand";
+                n %= 1000;
+            }
+
+            if(n / 100 > 0)
+            {
+                if (!string.IsNullOrEmpty(words))
+                    words += " ";
+
+                words += Number2Words(n / 100) + " hundred";
+                n %= 100;
+            }
+
+            if(n > 0)
+            {
+                if (!string.IsNullOrEmpty(words))
+                    words += " ";
+
+                if (n < 20)
+                    words += map[n];
+                else
+                {
+                    words += tens[n / 10];
+                    if (n % 10 > 0)
+                        words += "-" + map[n % 10];
+                }
+            }
+
+            return words;
+        }
+
+        
+    }
+}
---
+++ KataTests/Number2Words.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class NumberTranslationTest
+    {
+        [Test]
+        public void Number2WordsTest()
+        {
+            Assert.AreEqual("seven hundred ninety-three", NumberTranslation.Number2Words(793));
+            Assert.AreEqual("zero", NumberTranslation.Number2Words(0));
+            Assert.AreEqual("one", NumberTranslation.Number2Words(1));
+            Assert.AreEqual("three", NumberTranslation.Number2Words(3));
+            Assert.AreEqual("five", NumberTranslation.Number2Words(5));
+            Assert.AreEqual("eight", NumberTranslation.Number2Words(8));
+            Assert.AreEqual("three hundred one", NumberTranslation.Number2Words(301));
+            Assert.AreEqual("eight hundred", NumberTranslation.Number2Words(800));
+            Assert.AreEqual("six hundred fifty", NumberTranslation.Number2Words(650));
+            Assert.AreEqual("one thousand", NumberTranslation.Number2Words(1000));
+            Assert.AreEqual("one thousand three", NumberTranslation.Number2Words(1003));
+            Assert.AreEqual("seven thousand three hundred", NumberTranslation.Number2Words(7300));
+            Assert.AreEqual("one hundred seventy-nine thousand one hundred ninety-seven", NumberTranslation.Number2Words(179197));
+        }
+    }
+}
