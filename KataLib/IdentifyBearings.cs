7757344bec20609a80bc699f68c29d0045b3b5b5
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-25 18:44:19 +0300

    Identify bearings

---
+++ KataLib/IdentifyBearings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Bearing
+    {
+        public Bearing(int n)
+        {
+
+        }
+    }
+
+    public static class IdentifyBearings
+    {
+        public static Bearing IdentifyBb(Bearing[] bearings, Func<IEnumerable<Bearing>, long> weigh)
+        {
+            var heap = bearings.SelectMany((o, i) => o.GetSome(i+1)).ToList();
+            return bearings[weigh(heap) - heap.Count * 10 - 1];
+        }
+
+        public static Bearing[] GetSome(this Bearing b, int n)
+            => Enumerable.Range(1, n).Select(o => b).ToArray();
+
+
+        //public static Bearing IdentifyBb(Bearing[] bearings, Func<IEnumerable<Bearing>, long> weigh)
+        //{
+        //    return bearings[weigh(bearings.SelectMany(Enumerable.Repeat)) - 5 * bearings.Length * (bearings.Length - 1)];
+        //}
+    }
+}
