3468c452c569e70d204b52007c0f715431f586ef
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-10-30 15:18:13 +0300

    Count smileys

---
+++ KataLib/CountSmileys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class CountSmileysClass
+    {
+        public static int CountSmileys(string[] smileys)
+            => smileys.Count(o => o.GotSmiley());
+
+        public static bool GotSmiley(this string text)
+            => (text.First() == ':' || text.First() == ';')
+                    && (text.Last() == ')' || text.Last() == 'D')
+                    && (text.Length == 2 || text[1] == '~' || text[1] == '-');
+
+
+        //=> smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[~-]{0,1}[\)D]{1}$"));
+    }
+}
---
+++ KataTests/CountSmileys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KataLib;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    internal class CountSmileysTest
+    {
+        [Test]
+        public void BasicTest()
+        {
+            Assert.AreEqual(4, CountSmileysClass.CountSmileys(new string[] { ":D", ":~)", ";~D", ":)" }));
+            Assert.AreEqual(2, CountSmileysClass.CountSmileys(new string[] { ":)", ":(", ":D", ":O", ":;" }));
+            Assert.AreEqual(1, CountSmileysClass.CountSmileys(new string[] { ";]", ":[", ";*", ":$", ";-D" }));
+            Assert.AreEqual(0, CountSmileysClass.CountSmileys(new string[] { ";", ")", ";*", ":$", "8-D" }));
+        }
+    }
+}
