52837e4f0991ceed68f2e6a39db7618f22b41bca
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-08 23:03:46 +0200

    spongebob memes

---
+++ KataLib/Spongebob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Spongebob
+    {
+        public static string SpongeMeme(string sentence)
+            => String.Concat(sentence.Select((c, i) => i % 2 == 0 ? char.ToUpper(c) : char.ToLower(c)));
+    }
+}
