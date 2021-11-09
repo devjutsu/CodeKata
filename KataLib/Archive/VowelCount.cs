d977fac81127b8bf58622c8dcb6593d265bb0dc8
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-09 21:57:23 +0200

    Vowel count

---
+++ KataLib/Archive/VowelCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib.Archive
+{
+    public class VowelCount
+    {
+        public static int GetVowelCount(string str)
+            => str.Count(c => "aeiou".Contains(c));
+    }
+}
---
+++ KataTests/Archive/VowelCount.cs
@@ -0,0 +1,19 @@
+﻿using KataLib.Archive;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests.Archive
+{
+    internal class VowelCountTest
+    {
+        [Test]
+        public void TestCase1()
+        {
+            Assert.AreEqual(5, VowelCount.GetVowelCount("abracadabra"), "Nope!");
+        }
+    }
+}
