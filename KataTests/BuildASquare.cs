856ae94db30311b0856cb504c7a214af7dc0212f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-11 23:10:31 +0200

    build a square

---
+++ KataTests/BuildASquare.cs
@@ -0,0 +1,22 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class BuildASquareTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("", BuildASquare.GenerateShape(0));
+            Assert.AreEqual("+", BuildASquare.GenerateShape(1));
+            Assert.AreEqual("++\n++", BuildASquare.GenerateShape(2));
+            Assert.AreEqual("+++\n+++\n+++", BuildASquare.GenerateShape(3));
+        }
+    }
+}
