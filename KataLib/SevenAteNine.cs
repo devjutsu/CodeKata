e543aba827ff7865a611d22f164d226421f929e6
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-12 17:35:51 +0200

    seven ate nine

---
+++ KataLib/SevenAteNine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace KataLib
+{
+    public class SevenAte9
+    {
+        public static string SevenAteNine(string str)
+            => str.Replace("797", "77").Replace("797", "77");
+    }
+}
