8d36e6c85853855578109cd63d562746217b1e5d
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-14 23:48:44 +0200

    you are a square

---
+++ KataTests/YouAreSquare.cs
@@ -0,0 +1,24 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class YouAreSquareTste
+    {
+        [Test]  
+        public static void ShouldWorkForSomeExamples()
+        {
+            Assert.AreEqual(false, YouAreSquare.IsSquare(177870), "aren't square numbers");
+            Assert.AreEqual(false, YouAreSquare.IsSquare(-1), "negative numbers aren't square numbers");
+            Assert.AreEqual(false, YouAreSquare.IsSquare(3), "3 isn't a square number");
+            Assert.AreEqual(true, YouAreSquare.IsSquare(4), "4 is a square number");
+            Assert.AreEqual(true, YouAreSquare.IsSquare(25), "25 is a square number");
+            Assert.AreEqual(false, YouAreSquare.IsSquare(26), "26 isn't a square number");
+        }
+    }
+}
