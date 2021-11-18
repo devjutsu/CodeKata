b1fbb907b928b46118e97f923f2a0ba92bcc68e7
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-18 20:59:12 +0200

    dry potatoes

---
+++ KataTests/DryPotatoes.cs
@@ -0,0 +1,29 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    [TestFixture]
+    public static class DryPotatoesTests
+    {
+
+        private static void dotest(int p0, int w0, int p1, int expected)
+        {
+            Assert.AreEqual(expected, DryPotatoes.Potatoes(p0, w0, p1));
+        }
+
+        [Test]
+        public static void test()
+        {
+            Console.WriteLine("Fixed Tests: Potatoes");
+            dotest(99, 100, 98, 50);
+            dotest(93, 129, 91, 100);
+            dotest(82, 127, 80, 114);
+        }
+    }
+}
