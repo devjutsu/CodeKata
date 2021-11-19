c1663eed756b2814e1406b31ce1ee20853c4e64e
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-19 21:12:09 +0200

    Greatest common divisor

---
+++ KataLib/GreatestCommonDivisor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class GreatestCommonDivisor
+    {
+        public static int Gcd(int a, int b)
+            => b == 0 ? a : Gcd(b, a % b);
+    }
+}
