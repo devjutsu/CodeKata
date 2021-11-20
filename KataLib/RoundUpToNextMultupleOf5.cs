6835e1bb4cbab462306d365cd08ce8e7d8d36a8c
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-20 20:17:09 +0200

    Round to next multuple of 5

---
+++ KataLib/RoundUpToNextMultupleOf5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class RoundUpToNextMultupleOf5
+    {
+        public static int RoundToNext5(int n)
+            => n % 5 == 0 ? n : RoundToNext5(n + 1);
+    }
+}
