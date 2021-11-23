72c6273217c553c176f5117e1fe8f0ec69f4998d
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-23 13:16:14 +0200

    Catalan number

---
+++ KataLib/Catalan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Catalan
+    {
+        public static BigInteger NthCatalanNumber(int n)
+        {
+            BigInteger cat = 1;
+
+            for (int i = 1; i < n+1; i++)
+            {
+                cat *= (4 * i - 2);
+                cat /= (i + 1);
+            }
+            return cat;
+        }
+    }
+}
---
+++ KataTests/Catalan.cs
@@ -0,0 +1,46 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class CatalanTest
+    {
+        [Test]
+        public void TestNumberZero()
+        {
+            BigInteger number = 1;
+            Assert.AreEqual(number, Catalan.NthCatalanNumber(0), "Remember to use zero-based numbering");
+
+        }
+        [Test]
+        public void TestNumberOne()
+        {
+            BigInteger number = 1;
+            Assert.AreEqual(number, Catalan.NthCatalanNumber(1), "Remember to return BigInteger");
+        }
+        [Test]
+        public void TestNumberTwo()
+        {
+            BigInteger number = 2;
+            Assert.AreEqual(number, Catalan.NthCatalanNumber(2), "Remember to return BigInteger");
+        }
+        [Test]
+        public void TestNumberThree()
+        {
+            BigInteger number = 5;
+            Assert.AreEqual(number, Catalan.NthCatalanNumber(3), "Remember to return BigInteger");
+        }
+        [Test]
+        public void TestNumberFour()
+        {
+            BigInteger number = 14;
+            Assert.AreEqual(number, Catalan.NthCatalanNumber(4), "Remember to return BigInteger");
+        }
+    }
+}
