b14782b00dc3ac404236f0c6530aba4179ec949e
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-24 21:52:24 +0200

    Sum of a sequence

---
+++ KataLib/SumOfASequence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class SumOfASequence
+    {
+        public static int SequenceSum(int start, int end, int step)
+        => start > end ? 0
+            : Enumerable.Range(0, (end - start) / step + 1)
+                      .Select(i => start + step * i).Sum();
+    }
+}
