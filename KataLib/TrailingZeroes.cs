6720bd5ad2a9603bee8a766aa7370122ec09e4ad
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-26 17:56:25 +0200

    Trailing zeroes

---
+++ KataLib/TrailingZeroes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class TrailingZeroes
+    {
+        public static int TrailingZeros(int n)
+        {
+            var zeroes = 0;
+            for (int i = 5; n / i >= 1; i *= 5)
+                zeroes += n / i;
+            return zeroes;
+        }
+    }
+}
