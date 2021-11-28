73bfeb4b24e340120fd6c21c39188e0b87acd0f5
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-28 19:39:31 +0200

    Found by half step

---
+++ KataLib/RoundByHalfStep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class RoundByHalfStep
+    {
+        public static double Solution(double n)
+            => ((n - Math.Floor(n)) < .25) ? Math.Floor(n)
+                : ((n - Math.Floor(n)) < .75) ? Math.Floor(n) + .5
+                    : Math.Floor(n) + 1;
+
+
+        //public static double Solution(double n) 
+        //    => Math.Round(n * 2) / 2;
+    }
+}
---
+++ KataTests/RoundByHalfStep.cs
@@ -0,0 +1,27 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class RoundByHalfStepTest
+    {
+        private static object[] sampleTestCases = new object[]
+    {
+      new object[] {4.2, 4},
+      new object[] {4.4, 4.5},
+      new object[] {4.6, 4.5},
+      new object[] {4.8, 5},
+    };
+
+        [Test, TestCaseSource("sampleTestCases")]
+        public void SampleTest(double n, double expected)
+        {
+            Assert.AreEqual(expected, RoundByHalfStep.Solution(n));
+        }
+    }
+}
