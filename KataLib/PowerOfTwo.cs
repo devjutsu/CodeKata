66a321f6bffee1a3c4285e2aa7b09feb3583922d
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-29 19:46:56 +0200

    Power of two

---
+++ KataLib/PowerOfTwo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class PowerOfTwoKata
+    {
+        public static bool PowerOfTwo(int n)
+            => Convert.ToString(n, 2).Count(o => o == '1') == 1;
+    }
+}
---
+++ KataTests/PowerOfTwo.cs
@@ -0,0 +1,26 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class PowerOfTwoTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(2).Returns(true);
+                yield return new TestCaseData(4096).Returns(true);
+                yield return new TestCaseData(5).Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool SampleTest(int n) => PowerOfTwoKata.PowerOfTwo(n);
+    }
+}
