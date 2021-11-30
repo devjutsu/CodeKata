bcfbd6b6d130e810870c1cc96c2875104d96ea9d
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-11-30 13:12:44 +0200

    numbers expanded form

---
+++ KataLib/NumbersExpandedForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class NumbersExpandedForm
+    {
+        public static string ExpandedForm(long num)
+            => num.ToString()
+                        .Reverse()
+                        .Select((o, i) => int.Parse(o.ToString()) * Math.Pow(10, i))
+                        .Where(o => o > 0)
+                        .Reverse()
+                        .Select(o => o.ToString()).Aggregate((a, b) => $"{a} + {b}");
+    }
+}
