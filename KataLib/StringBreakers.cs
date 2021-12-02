56ac6521737c89c0f895c079106615f73b37f23c
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-02 10:03:03 +0200

    String breakers

---
+++ KataLib/StringBreakers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class StringBreakerz
+    {
+        public static string StringBreakers(int n, string str)
+            => string.Join("\n", str.Replace(" ", "")
+                            .Select((c, i) => new { c, i })
+                            .GroupBy(o => o.i / n)
+                            .Select(o => string.Concat(o.Select(z => z.c))));
+    }
+}
---
+++ KataTests/StringBreakers.cs
@@ -0,0 +1,19 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class StringBreakersTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Thisi" + "\n" + "sanex" + "\n" + "ample" + "\n" + "strin" + "\n" + "g", StringBreakerz.StringBreakers(5, "This is an example string"));
+        }
+    }
+}
