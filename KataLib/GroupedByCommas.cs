0824a01c86d1ea109ee356a14021fc297a61e35e
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-04 16:41:58 +0200

    Grouped by commas

---
+++ KataLib/GroupedByCommas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class GroupedByCommas
+    {
+        public static string GroupByCommas(int n)
+            => string.Join(",", n.ToString()
+                        .Reverse()
+                        .Select((c, i) => new { c, i })
+                        .GroupBy(x => x.i / 3)
+                        .Select(o => string.Concat(o.Select(z => z.c).Reverse()))
+                        .Reverse()
+                        .ToList());
+
+        //public static string GroupByCommas(int n) => $"{n:n0}";
+    }
+}
