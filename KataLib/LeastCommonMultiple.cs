f00be4deea47fd5678a192bdf94bf33bf54af113
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-08 11:51:12 +0200

    Least common multiple

---
+++ KataLib/LeastCommonMultiple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class LeastCommonMultiple
+    {
+        public static int Lcm(List<int> nums)
+            => nums.Count == 0
+            ? 1
+            : nums.Aggregate((a, b) => (a * b) / GCD(a, b)); 
+
+        static int GCD(int a, int b)
+            => b == 0 ? a : GCD(b, a % b);
+    }
+}
