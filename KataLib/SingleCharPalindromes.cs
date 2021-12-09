b23330ee6f2714b4b7438cc70621aaaaafc2dcf4
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-09 14:09:36 +0200

    Single char palindromes

---
+++ KataLib/SingleCharPalindromes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public static class SingleCharPalindromes
+    {
+        public static string solve(string s)
+            => s.IsPalindrome() ? "OK"
+            : Enumerable.Range(0, s.Length)
+                .Select(i => s.Substring(0, i) + s.Substring(i + 1))
+                .Any(o => o.IsPalindrome()) ? "remove one"
+            : "not possible";
+
+        public static bool IsPalindrome(this string s)
+           => s == string.Concat(s.Reverse());
+    }
+}
---
+++ KataTests/SingleCharPalindromes.cs
@@ -0,0 +1,25 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class SingleCharPalindromesTest
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual("OK", SingleCharPalindromes.solve("abba"));
+            Assert.AreEqual("remove one", SingleCharPalindromes.solve("abbaa"));
+            Assert.AreEqual("not possible", SingleCharPalindromes.solve("abbaab"));
+            Assert.AreEqual("remove one", SingleCharPalindromes.solve("madmam"));
+            Assert.AreEqual("not possible", SingleCharPalindromes.solve("raydarm"));
+            Assert.AreEqual("OK", SingleCharPalindromes.solve("hannah"));
+
+        }
+    }
+}
