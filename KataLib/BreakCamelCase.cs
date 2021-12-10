e70b62266a4f90aeab8c7b826e0c4d76d168ac2c
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-10 13:18:33 +0200

    Break camel case

---
+++ KataLib/BreakCamelCase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class BreakCamel
+    {
+        public static string BreakCamelCase(string str)
+        => string.Concat(Enumerable.Range(0, str.Length - 1)
+                                        .Select(i => char.IsLetterOrDigit(str[i]) && char.IsUpper(str[i + 1])
+                                                ? str[i].ToString() + " " : str[i].ToString())) + str[^1];
+
+        //public static string BreakCamelCase(string str) => new Regex("([A-Z])").Replace(str, " $1");
+
+        // => string.Concat(str.Select(c => Char.IsUpper(c) ? " " + c : c.ToString()));
+
+    }
+}
---
+++ KataTests/BreakCamelCase.cs
@@ -0,0 +1,25 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class BreakCamelCaseTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("camelCasing").Returns("camel Casing");
+                yield return new TestCaseData("camelCasingTest").Returns("camel Casing Test");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string str) => BreakCamel.BreakCamelCase(str);
+    }
+}
