0d4d6386b846d40433a3cb4c9ee8dde96ecd38c5
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-11 12:29:41 +0200

    Count chars in string

---
+++ KataLib/CountCharsInString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class CountCharsInString
+    {
+        public static Dictionary<char, int> Count(string str)
+            => str.GroupBy(x => x)
+                    .ToDictionary(x => x.Key, x => x.Count());
+    }
+}
