8958486c4028468c0d21cd16e35f06b97762fe08
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-16 18:35:52 +0200

    John meeting

---
+++ KataLib/JohnMeeting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class JohnMeeting
+    {
+        public static string Meeting(string s)
+            => string.Concat(s.ToUpper().Split(';')
+                            .Select(o => o.Split(':'))
+                            .OrderBy(o => o[1])
+                            .ThenBy(o => o[0])
+                            .Select(o => $"({o[1]}, {o[0]})"));
+    }
+}
