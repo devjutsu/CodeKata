407940e2ba7386aa604f7fafda158f45cf4e0bd1
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-17 18:31:09 +0200

    Schrodinger's bool

---
+++ KataTests/OmniBool.cs
@@ -0,0 +1,20 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class OmniBoolTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.IsTrue(OmniBool.omnibool == true);
+            Assert.IsTrue(OmniBool.omnibool == false);
+        }
+    }
+}
