610f1e3acb41dddaad1634f82850c5e77f53c087
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-18 19:07:20 +0200

    Backspace in string

---
+++ KataLib/BackspacesInString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class BackspacesInString
+    {
+        public static string CleanString(string s)
+        {
+            while(s.Contains("#"))
+            {
+                var i = s.IndexOf("#");
+                if (i > 0)
+                    s = s.Remove(i - 1, 2);
+                else 
+                    s = s.Remove(i, 1);
+            }
+            return s;
+        }
+
+        // s.Aggregate("", (a, c) => c == '#' ? a.Any() ? a[..^1] : a : a + c);
+
+        // System.Text.RegularExpressions.Regex.Replace(s, "((?<c>[^#])|(?<-c>#))*(?(c)(?!))#*", "");
+    }
+}
---
+++ KataTests/BackspacesInStringTest.cs
@@ -0,0 +1,20 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class BackspacesInStringTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual("ac", BackspacesInString.CleanString("abc#d##c"));
+            Assert.AreEqual("", BackspacesInString.CleanString("abc####d##c#"));
+        }
+    }
+}
