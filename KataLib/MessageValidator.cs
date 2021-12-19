e8aa79851e8a222054cea207b54980944c930f8f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-19 16:32:10 +0200

    Message validator

---
+++ KataLib/MessageValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace KataLib
+{
+    public class MessageValidator
+    {
+        public static bool isAValidMessage(string message)
+        => Regex.Matches(message, @"(?<d>[\d]+)(?<w>[\D]+)?")
+                .All(m => int.Parse(m.Groups["d"].Value) == m.Groups["w"].Value.Length);
+
+        //public static bool isAValidMessage(string message)
+        //{
+        //    if (string.IsNullOrEmpty(message))
+        //        return true;
+
+        //    var hz = Regex.Matches(message, "\\d+|\\D+")
+        //            .Select(o => o.Value)
+        //            .ToList();
+
+        //    for(var i = 0; i < hz.Count; i++)
+        //    {
+        //        int.TryParse(hz[i], out int res);
+        //        if(res != 0)
+        //        {
+        //            if (hz.Count < i+2 || hz[i + 1].Count() != res)
+        //                return false;
+        //            else
+        //                i++;
+        //        }
+        //    }
+
+        //    return true;
+        //}
+    }
+}
---
+++ KataTests/MessageValidatorTest.cs
@@ -0,0 +1,49 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class MessageValidatorTest
+    {
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual(true, MessageValidator.isAValidMessage("3hey5hello2hi"));
+        }
+
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual(true, MessageValidator.isAValidMessage("4code13hellocodewars"));
+        }
+
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(false, MessageValidator.isAValidMessage("3hey5hello2hi5"));
+        }
+
+        [Test]
+        public void test4()
+        {
+            Assert.AreEqual(false, MessageValidator.isAValidMessage("code4hello5"));
+        }
+
+        [Test]
+        public void test5()
+        {
+            Assert.AreEqual(true, MessageValidator.isAValidMessage("1a2bb3ccc4dddd5eeeee"));
+        }
+
+        [Test]
+        public void test6()
+        {
+            Assert.AreEqual(true, MessageValidator.isAValidMessage(""));
+        }
+    }
+}
