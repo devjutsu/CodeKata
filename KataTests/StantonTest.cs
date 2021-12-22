a279e127f611a84a3b61d273c94c39bd11b641ca
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-22 20:15:25 +0200

    Stanton measure

---
+++ KataTests/StantonTest.cs
@@ -0,0 +1,19 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class StandonTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(3, Stanton.StantonMeasure(new int[] { 1, 4, 3, 2, 1, 2, 3, 2 }));
+        }
+    }
+}
