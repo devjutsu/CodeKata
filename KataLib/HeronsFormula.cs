cb61b0335d2b4fad4289c21b1df7cc84f651963b
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-26 18:44:59 +0200

    Heron's formula

---
+++ KataLib/HeronsFormula.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class HeronsFormula
+    {
+        public static double heron(double a, double b, double c)
+        {
+            var s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+    }
+}
---
+++ KataTests/HeronsFormula.cs
@@ -0,0 +1,19 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class HeronsFormulaTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(6.0, HeronsFormula.heron(3, 4, 5));
+        }
+    }
+}
