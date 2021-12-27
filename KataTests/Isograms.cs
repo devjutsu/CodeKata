4121828e5805712245bba2d634dd591538535ac4
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-27 11:21:34 +0200

    Isograms

---
+++ KataTests/Isograms.cs
@@ -0,0 +1,31 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class IsogramsTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Dermatoglyphics").Returns(true);
+                yield return new TestCaseData("isogram").Returns(true);
+                yield return new TestCaseData("moose").Returns(false);
+                yield return new TestCaseData("isIsogram").Returns(false);
+                yield return new TestCaseData("aba").Returns(false);
+                yield return new TestCaseData("moOse").Returns(false);
+                yield return new TestCaseData("thumbscrewjapingly").Returns(true);
+                yield return new TestCaseData("").Returns(true);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => Isograms.IsIsogram(str);
+    }
+}
