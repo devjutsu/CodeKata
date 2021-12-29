f1cc474cc626788b2a81bd7cc00aa7ee40c1df75
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-29 16:25:52 +0200

    Sum strings

---
+++ KataLib/SumStrings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace KataLib
+{
+    public class SumStrings
+    {
+        public static string sumStrings(string a, string b)
+        => (BigInteger.Parse(!String.IsNullOrWhiteSpace(a) ? a : "0")
+                + BigInteger.Parse(!String.IsNullOrWhiteSpace(b) ? b : "0")).ToString();
+    }
+}
---
+++ KataTests/SumStrings.cs
@@ -0,0 +1,25 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class SumStringsTest
+    {
+        [Test]
+        public void Given123And456Returns579()
+        {
+            Assert.AreEqual("579", SumStrings.sumStrings("123", "456"));
+        }
+
+        [Test]
+        public void LongTest()
+        {
+            Assert.AreEqual("131151201344081895336534324866", SumStrings.sumStrings("50095301248058391139327916261", "81055900096023504197206408605"));
+        }
+    }
+}
