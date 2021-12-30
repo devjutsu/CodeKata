34e5b758f08317f1e35bde6ea3d23d68c5a179fc
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-30 17:22:28 +0200

    Digitalize

---
+++ KataLib/Digitalize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class Digitalize
+    {
+        public static int[] digitize(int n)
+            => n.ToString().Select(_ => int.Parse(_.ToString())).ToArray();
+    }
+}
