2e8d2a1642fa716ce3981202f0d3c77f860eed04
Alberts Bušinskis <alberts.businskis@erppro.lv>
2021-12-31 14:48:52 +0200

    Strip comments

---
+++ KataLib/StripComments.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class StripCommentsSolution
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            return string
+              .Join("\n", text.Split("\n")
+              .Select(x => x.Split(commentSymbols, StringSplitOptions.None)[0]
+              .TrimEnd(' ')));
+        }
+
+        //public static string StripComments(string text, string[] commentSymbols)
+        //{
+        //    var tmp = text.Split("\n");
+        //    foreach(var s in commentSymbols)
+        //    {
+        //        for(var i = 0; i < tmp.Length; i++)
+        //        {
+        //            var cutTo = tmp[i].IndexOf(s);
+        //            if(cutTo != -1)
+        //            {
+        //                tmp[i] = tmp[i].Substring(0, cutTo);
+        //            }
+        //        }
+        //    }
+        //    return String.Join("\n", tmp.Select(o => o.TrimEnd()).ToArray()); ;
+        //}
+    }
+}
---
+++ KataTests/StripComments.cs
@@ -0,0 +1,24 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class StripCommentsTest
+    {
+        [Test]
+        public void StripComments()
+        {
+            Assert.AreEqual(
+                    "apples, pears\ngrapes\nbananas",
+                    StripCommentsSolution.StripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new string[] { "#", "!" }));
+
+            Assert.AreEqual("a\nc\nd", StripCommentsSolution.StripComments("a #b\nc\nd $e f g", new string[] { "#", "$" }));
+
+        }
+    }
+}
