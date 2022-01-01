b17db18dc4a6689489f7d83f60445d12dd0662cf
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-01 17:27:43 +0200

    TicTacToe checker

---
+++ KataTests/TicTacToeChecker.cs
@@ -0,0 +1,29 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class TicTacToeCheckerTest
+    {
+        private TicTacToeChecker tictactoe = new TicTacToeChecker();
+
+        [Test]
+        public void test1()
+        {
+            int[,] board = new int[,] { { 1, 1, 1 }, { 0, 2, 2 }, { 0, 0, 0 } };
+            Assert.AreEqual(1, tictactoe.IsSolved(board));
+        }
+
+        [Test]
+        public void test2()
+        {
+            int[,] board = new int[,] { { 1, 2, 0 }, { 0, 1, 2 }, { 0, 0, 1 } };
+            Assert.AreEqual(1, tictactoe.IsSolved(board));
+        }
+    }
+}
