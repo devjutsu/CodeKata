ae164d47f1f5355604d98d383383e335445935d9
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-04 20:03:17 +0200

    Flip bit binary operations 101

---
+++ KataTests/BinaryOperations1.cs
@@ -0,0 +1,34 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class BinaryOperations1Test
+    {
+        [Test]
+        public void test0()
+        {
+            Assert.AreEqual(255, BinaryOperations1.FlipBit(127, 8));
+        }
+        [Test]
+        public void test1()
+        {
+            Assert.AreEqual(7, BinaryOperations1.FlipBit(15, 4));
+        }
+        [Test]
+        public void test2()
+        {
+            Assert.AreEqual(1, BinaryOperations1.FlipBit(0, 1));
+        }
+        [Test]
+        public void test3()
+        {
+            Assert.AreEqual(14, BinaryOperations1.FlipBit(15, 1));
+        }
+    }
+}
