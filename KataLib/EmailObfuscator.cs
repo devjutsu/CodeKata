b96236fb8236673b5da275429c4f2e8babad71df
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-05 16:11:58 +0200

    Email obfuscator

---
+++ KataLib/EmailObfuscator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class EmailObfuscator
+    {
+        public static string Obfuscate(string email)
+        {
+            return email.Replace(".", " [dot] ").Replace("@", " [at] ");
+        }
+    }
+}
