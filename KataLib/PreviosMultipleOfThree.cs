b998165003aa1de808aca5a064f6ca5603690478
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-08 19:26:06 +0200

    Previous multiple of three

---
+++ KataLib/PreviosMultipleOfThree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class PreviosMultipleOfThree
+    {
+        public static int? PreviousMultipleOfThree(int n)
+            => n % 3 == 0 ? n
+                : n.ToString().Length == 1 ? null
+                : PreviousMultipleOfThree(int.Parse(n.ToString().Substring(0, n.ToString().Length - 1)));
+    }
+}
