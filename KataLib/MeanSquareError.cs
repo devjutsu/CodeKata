1c2d3c1d02112f9e340b0c6f1f542f15245d77d2
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-09 18:52:50 +0200

    Mean square error

---
+++ KataLib/MeanSquareError.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class MeanSquareError
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+            => firstArray.Select((x, i) => x - secondArray[i])
+                    .Select(x => Math.Pow(x, 2)).Average();
+
+        //public static double Solution(int[] xs, int[] ys) 
+        //    => xs.Zip(ys, (x, y) => (x - y) * (x - y))
+        //            .Average();
+    }
+}
---
+++ KataTests/MeanSquareError.cs
@@ -0,0 +1,21 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class MeanSquareErrorTest
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual(9, MeanSquareError.Solution(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 }));
+            Assert.AreEqual(16.5, MeanSquareError.Solution(new int[] { 10, 20, 10, 2 }, new int[] { 10, 25, 5, -2 }));
+            Assert.AreEqual(1, MeanSquareError.Solution(new int[] { 0, -1 }, new int[] { -1, 0 }));
+        }
+    }
+}
