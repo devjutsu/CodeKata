83d6d4fb4a56f8c61f805ee4fdab8d294a662718
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-10 19:47:23 +0200

    Next bigger number

---
+++ KataLib/NextBiggerNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class NextBiggerNumberClass
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            var arr = n.ToString().Select(o => int.Parse(o.ToString())).ToArray();
+            var i = 0;
+
+            for(i = arr.Count() - 1; i > 0; i--)
+                if(arr[i] > arr[i - 1])
+                    break;
+
+            if (i == 0)
+                return -1;
+            else
+            {
+                var x = arr[i - 1];
+                var min = i;
+
+                for (int j = i+1; j < arr.Length; j++)
+                    if (arr[j] > x && arr[j] < arr[min])
+                        min = j;
+
+                var tmp = arr[i - 1];
+                arr[i - 1] = arr[min];
+                arr[min] = tmp;
+
+                return long.Parse(String.Concat(arr[..i].Concat(arr[i..].OrderBy(x => x))));
+            }
+        }
+
+        //public static long NextBiggerNumber(long n)
+        //{
+        //    String str = GetNumbers(n);
+        //    for (long i = n + 1; i <= long.Parse(str); i++)
+        //    {
+        //        if (GetNumbers(n) == GetNumbers(i))
+        //        {
+        //            return i;
+        //        }
+        //    }
+        //    return -1;
+        //}
+        //public static string GetNumbers(long number)
+        //{
+        //    return string.Join("", number.ToString().ToCharArray().OrderByDescending(x => x));
+        //}
+    }
+}
---
+++ KataTests/NextBiggerNumber.cs
@@ -0,0 +1,24 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class NextBiggerNumberTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1608305302, NextBiggerNumberClass.NextBiggerNumber(1608305230));
+            Assert.AreEqual(2071, NextBiggerNumberClass.NextBiggerNumber(2017));
+            Assert.AreEqual(21, NextBiggerNumberClass.NextBiggerNumber(12));
+            Assert.AreEqual(531, NextBiggerNumberClass.NextBiggerNumber(513));
+            Assert.AreEqual(441, NextBiggerNumberClass.NextBiggerNumber(414));
+            Assert.AreEqual(414, NextBiggerNumberClass.NextBiggerNumber(144));
+        }
+    }
+}
