d8718b7ddf79ebac4e64965e9cec25f46e5d206f
Alberts Bušinskis <alberts.businskis@erppro.lv>
2022-01-11 13:38:18 +0200

    Bumps on the road

---
+++ KataLib/BumpsOnTheRoad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class BumpsOnTheRoad
+    {
+        public static string Bump(string input)
+            => input.Count(o => o == 'n') > 15 ? "Car Dead" : "Woohoo!";
+    }
+}
---
+++ KataTests/BumpsOnTheRoad.cs
@@ -0,0 +1,21 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class BumpsOnTheRoadTest
+    {
+        [TestCase("n", "Woohoo!")]
+        [TestCase("_nnnnnnn_n__n______nn__nn_nnn", "Car Dead")]
+        [TestCase("______n___n_", "Woohoo!")]
+        public void BasicTests(string input, string expected)
+        {
+            Assert.That(BumpsOnTheRoad.Bump(input), Is.EqualTo(expected));
+        }
+    }
+}
