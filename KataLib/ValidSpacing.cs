d0a41f30fef03451d28415766df3192ebd3762f7
Albert Garuda <algarud@gmail.com>
2022-01-15 19:33:46 +0200

    Valid spacing

---
+++ KataLib/ValidSpacing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class ValidSpacingClass
+    {
+        public static bool ValidSpacing(string s)
+            => s.Trim() == s && !s.Contains("  ");
+    }
+}
---
+++ KataTests/ValidSpacing.cs
@@ -0,0 +1,28 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class ValidSpacingTest
+    {
+        [Test(Description = "Sample tests")]
+        public void SampleTests()
+        {
+            Test(true, "Hello world");
+            Test(false, " Hello world");
+            Test(false, "Hello world ");
+            Test(true, "Hello");
+            Test(true, "Helloworld");
+        }
+
+        private void Test(bool solution, string input)
+        {
+            Assert.AreEqual(solution, ValidSpacingClass.ValidSpacing(input), $"Incorrect answer for input \"{input}\"");
+        }
+    }
+}
