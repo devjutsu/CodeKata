8369db277b905cb840e0f346c5ce2b6b2efcf8c6
Albert Garuda <algarud@gmail.com>
2022-03-03 21:42:06 +0200

    Number pairs

---
+++ KataLib/NumberPairs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class NumberPairs
+    {
+        public static int[] getLargerNumbers(int[] a, int[] b)
+            => a.Select((a, i) => Math.Max(a, b[i])).ToArray();
+    }
+}
