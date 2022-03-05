e173d84f0b87fc5a26c502d36e9508d49eaf9b11
Albert Garuda <algarud@gmail.com>
2022-03-05 21:37:20 +0200

    By 3

---
+++ KataLib/By3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace KataLib
+{
+    public class By3
+    {
+        public static bool DivisibleByThree(string n)
+            => BigInteger.Parse(n) % 3 == 0;
+    }
+}
---
+++ KataTests/By3.cs
@@ -0,0 +1,21 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class By3Test
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(By3.DivisibleByThree("1891009"), Is.EqualTo(false));
+            Assert.That(By3.DivisibleByThree("00009"), Is.EqualTo(true));
+            Assert.That(By3.DivisibleByThree("57"), Is.EqualTo(true));
+        }
+    }
+}
