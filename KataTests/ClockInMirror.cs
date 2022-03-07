ca18f266689dedb540c6933d91f8c28929ac5528
Albert Garuda <algarud@gmail.com>
2022-03-07 23:26:17 +0200

    Clock in mirror

---
+++ KataTests/ClockInMirror.cs
@@ -0,0 +1,43 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class ClockInMirrorTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            StringAssert.AreEqualIgnoringCase("06:35", ClockInMirror.WhatIsTheTime("05:25"));
+        }
+
+        [Test]
+        public void SecondTest()
+        {
+            StringAssert.AreEqualIgnoringCase("11:59", ClockInMirror.WhatIsTheTime("12:01"));
+        }
+
+        [Test]
+        public void ThirdTest()
+        {
+            StringAssert.AreEqualIgnoringCase("12:02", ClockInMirror.WhatIsTheTime("11:58"));
+        }
+
+        [Test]
+        public void FourhTest()
+        {
+            StringAssert.AreEqualIgnoringCase("12:00", ClockInMirror.WhatIsTheTime("12:00"));
+        }
+
+        [Test]
+        public void FifthTest()
+        {
+            StringAssert.AreEqualIgnoringCase("02:00", ClockInMirror.WhatIsTheTime("10:00"));
+        }
+    }
+}
