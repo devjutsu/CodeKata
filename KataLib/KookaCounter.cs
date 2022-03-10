663f34f8be98da024d958140e7cc0276a6ae192c
Albert Garuda <algarud@gmail.com>
2022-03-10 21:23:19 +0200

    Kooka counter

---
+++ KataLib/KookaCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace KataLib
+{
+    public class KookaCounterClass
+    {
+        public static int KookaCounter(string laughing) 
+            => Regex.Matches(laughing, "(Ha)+|(ha)+").Count;
+
+        //public static int KookaCounter(string laughing)
+        //{
+        //    if (string.IsNullOrEmpty(laughing))
+        //        return 0;
+        //    var seq = laughing.Where(o => o != 'a').ToArray();
+        //    var acc = 1;
+        //    for(var i = 0; i<seq.Count()-1; i++)
+        //    {
+        //        if(seq[i] != seq[i+1])
+        //            acc++;
+        //    }
+
+        //    return acc;
+        //}
+    }
+}
---
+++ KataTests/KookaCounter.cs
@@ -0,0 +1,27 @@
+﻿using KataLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataTests
+{
+    internal class KookaCounterTest
+    {
+        private static object[] Basic_Test_Cases = new object[]
+        {
+          new object[] {String.Empty, 0},
+          new object[] {"hahahahaha", 1},
+          new object[] {"hahahahahaHaHaHa", 2},
+          new object[] {"HaHaHahahaHaHa", 3}
+        };
+
+        [Test, TestCaseSource(typeof(SolutionTest), "Basic_Test_Cases")]
+        public void Basic_Test(string laughing, int expected)
+        {
+            Assert.AreEqual(expected, KookaCounterClass.KookaCounter(laughing));
+        }
+    }
+}
